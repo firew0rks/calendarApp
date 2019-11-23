1d8b6cb145b073bbcc7f8dea3cbbc9af43cd4547
oscar.lau <oscar.lau@accenture.com>
2019-11-24 10:06:02 +1100

    removing the hardcoding

--- app/components/DayActivity.js
+++ app/components/DayActivity.js
@@ -1,26 +1,26 @@
-import React, {useState, useRef} from 'react';
-import {View, StyleSheet, Text, ScrollView, SafeAreaView} from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, StyleSheet, Text, ScrollView, SafeAreaView } from 'react-native';
 import Clock from './Clock';
 import NavBar from './NavBar';
 import moment from 'moment';
 import DayCard from './DayCard';
 
 export default function DayActivity(props) {
-  const {schedule} = props.screenProps;
+  const { schedule } = props.screenProps;
   const multi = useRef(null);
 
   const formattedDate = moment().format('D/M/YY');
+
 
   const [time, setTime] = useState(moment().format('h:mm a'));
   const [date, setDate] = useState(moment().format('MMMM Do YYYY '));
   // Get the current day's object array.
   const currentDay = schedule[formattedDate];
-
   return (
     <View style={styles.app}>
       <SafeAreaView style={styles.container}>
-        <NavBar style={{flex: 1}} props={props} goToDayPage={false} />
-        <View style={{height: 80}}>
+        <NavBar style={{ flex: 1 }} props={props} goToDayPage={false} />
+        <View style={{ height: 80 }}>
           <Clock time={time} setTime={setTime} date={date} setDate={setDate} />
         </View>
         <ScrollView>
